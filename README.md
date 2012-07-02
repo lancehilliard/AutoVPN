a5bba2408d360d36f9348d7d9442c7a35a91114d
M. Lance Hilliard <lance@digitalcreations.cc>
2012-07-02 10:48:25 -0500

    added VPN script

---
+++ README.md
@@ -0,0 +1,9 @@
+A batch file for automatically connecting to an MS VPN. Designed to be used as a Startup script.
+
+Recommendations for the VPN profile to which you connect:
+
+Disable two settings:
+
+- Display progress while connecting
+
+- Prompt for name and password, certificate, etc
---
+++ src/autoVPN.bat
@@ -0,0 +1,45 @@
+@echo off
+
+if "%AUTOVPN_VPN_NAME%"=="" goto Instructions
+if "%AUTOVPN_REMOTE_LAN_IP%"=="" goto Instructions
+
+set IP_ADDRESS_ACCESSIBLE_FROM_ALL_NETWORKS=8.8.8.8
+set VPN_NAME=%AUTOVPN_VPN_NAME%
+set IP_ADDRESS_ACCESSIBLE_ON_REMOTE_LAN=%AUTOVPN_REMOTE_LAN_IP%
+
+:VerifyNetworkAvailable
+cls
+echo Verifying we're on the local LAN now...
+timeout 1 >NUL 2>&1
+ping -n 1 %IP_ADDRESS_ACCESSIBLE_FROM_ALL_NETWORKS% | find "TTL" >NUL 2>&1
+if %errorlevel%==1 goto VerifyNetworkAvailable
+goto TestVpnConnection
+
+
+:ConnectToVpn
+echo.
+echo Connecting us to the VPN now...
+rasdial /disconnect >NUL 2>&1
+rasdial %VPN_NAME% | find /v "completed"
+
+
+:TestVpnConnection
+echo Checking if we can access the remote LAN now...
+ping -n 2 %IP_ADDRESS_ACCESSIBLE_ON_REMOTE_LAN% | find "TTL" >NUL 2>&1
+if %errorlevel%==1 goto ConnectToVpn
+
+echo We can access the remote LAN!
+echo We'll close this window in a few seconds...
+timeout 10
+goto End
+
+
+:Instructions
+echo ENVIRONMENT VARIABLES NOT FOUND!
+echo Create two environment variables before running this script:
+echo AUTOVPN_VPN_NAME: the name of the MS VPN profile this script should connect to
+echo AUTOVPN_REMOTE_LAN_IP: the IP address on the remote lan that's pingable upon successful VPN connection
+pause
+
+
+:End
